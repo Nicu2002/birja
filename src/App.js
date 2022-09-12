9255fbed1e2b9134640405a7407120f6790a8175
Nicu2002 <nicu.mercurial@bk.ru>
2022-09-12 12:01:48 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { webSocket } from 'rxjs/webSocket';
+import {useEffect, useState} from "react";
+import {auditTime, distinct, filter, map, scan,} from "rxjs/operators";
+import CanvasJSReact from './canvasjs.react';
+const CanvasJS = CanvasJSReact.CanvasJS;
+const CanvasJSChart = CanvasJSReact.CanvasJSChart;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [prices, setPrices] = useState([{x: 123456789, y: 19000}]);
+
+    const options = {
+        theme: "light2", // "light1", "dark1", "dark2"
+        animationEnabled: true,
+        zoomEnabled: true,
+        title: {
+            text: "BTC/BUSD"
+        },
+        data: [{
+            type: "area",
+            dataPoints: prices.length > 1 ? prices.slice(1,).map(item => item[Object.getOwnPropertyNames(item)[0]]): null
+        }]
+    }
+
+    useEffect(() => {
+        const subject = webSocket("wss://stream.binance.com:9443/ws")
+        subject.next({
+            method: "SUBSCRIBE",
+            params: ["btcbusd@ticker"],
+            id: 1
+        })
+        subject.asObservable()
+            .pipe(
+                auditTime(1000),
+                map(item => ({[item.E]: {x: item.E, y: +item.c}})),
+                scan((init, current) => [...init, current], [])
+            )
+            .subscribe({
+            next: msg => setPrices(msg),
+            error: err => console.log("error"),
+            complete: () => console.log('complete'),
+        });
+    }, [])
+
+    const renderPrices = () => prices.map(item => <div style={{backgroundColor: "red", height: "30px", width: `${(item - 19830)* 50}px`}} key={Math.random()}>{item}</div>)
+
+    return (
+        <>
+            <CanvasJSChart options={options}/>
+
+            <button>Buy {prices[prices.length - 1][Object.getOwnPropertyNames(prices[prices.length - 1])[0]].y}</button>
+        </>
+    );
 }
 
 export default App;
---
+++ src/index2.js
@@ -0,0 +1,13 @@
+import {useRef, useState} from "react";
+
+//TODO: CFDSAFAS
+const MyComp = () => {
+    const [state, setState] = useState();
+    const refff = useRef()
+    return (
+        <div>
+        </div>
+    );
+};
+
+export default MyComp;
